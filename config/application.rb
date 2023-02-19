8ec3718f8d0515581304802fdeed01e59dbdc2d3
Pravin Patil <redhairshanks@github.com>
2023-02-19 18:25:27 +0530

    initial configurations for cache, database, permissions

--- config/application.rb
+++ config/application.rb
@@ -8,15 +8,16 @@ Bundler.require(*Rails.groups)
 
 module Qquiz
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.load_defaults 6.1
+    config.eager_load_paths << Rails.root.join('lib')
+    config.time_zone = 'Asia/Kolkata'
+    config.active_record.default_timezone = :local
+    config.generators.system_tests = nil
+    config.x.app_config = config_for('custom/config')
+    config.x.permission = config_for('custom/permission')
+    config.autoload_paths += Dir[Rails.root.join('app', 'jobs', '**/')]
+    config.autoload_paths += Dir[Rails.root.join('app', 'models', '**/')]
+    config.autoload_paths += Dir[Rails.root.join('app', 'mailers', '**/')]
+    config.autoload_paths += Dir[Rails.root.join('app', 'services', '**/')]
   end
 end
